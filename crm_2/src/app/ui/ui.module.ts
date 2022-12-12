b6b4f139bc81a836d3e9ca7c5ce402e4d9bbf5dc
PierreGau <p.gauthier09@gmail.com>
2022-12-12 12:10:23 +0100

    ajout nouvelle ui

--- crm_2/src/app/ui/ui.module.ts
+++ crm_2/src/app/ui/ui.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiComponent } from './components/ui/ui.component';
 import { CoreModule } from '../core/core.module';
+import { Ui3Component } from './components/ui3/ui3.component';
 
 
 
 @NgModule({
   declarations: [
-    UiComponent
+    UiComponent,
+    Ui3Component
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    UiComponent
+    UiComponent,
+    Ui3Component
   ]
 })
 export class UiModule { }
