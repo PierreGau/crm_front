1b1af6294704e632d76af5e5a0fce13af03210e4
PierreGau <p.gauthier09@gmail.com>
2022-12-12 09:04:17 +0100

    aside qui s'ouvre et se ferme

--- crm_2/src/app/ui/components/ui/ui.component.ts
+++ crm_2/src/app/ui/components/ui/ui.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
-  styleUrls: ['./ui.component.scss']
+  styleUrls: ['./ui.component.scss'],
 })
 export class UiComponent {
+  public close: boolean = true;
 
+  constructor() {}
+
+  public toggle(): void {
+    this.close = !this.close;
+  }
 }
