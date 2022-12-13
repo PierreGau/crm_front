9b4ecb44fa45f106be4912fa6d4f31b402825e50
PierreGau <p.gauthier09@gmail.com>
2022-12-13 09:23:41 +0100

    correction mauvais import

--- crm_2/src/app/orders/orders.module.ts
+++ crm_2/src/app/orders/orders.module.ts
@@ -1,23 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { OrdersRoutingModule } from './orders-routing.module';
-import { PageListOrderComponent } from './views/page-list-order/page-list-order.component';
-import { PageListOrdersComponent } from './views/page-list-orders/page-list-orders.component';
 import { PageAddOrderComponent } from './views/page-add-order/page-add-order.component';
 import { PageEditOrderComponent } from './views/page-edit-order/page-edit-order.component';
-
+import { PageListOrdersComponent } from './views/page-list-orders/page-list-orders.component';
 
 @NgModule({
   declarations: [
-    PageListOrderComponent,
     PageListOrdersComponent,
     PageAddOrderComponent,
-    PageEditOrderComponent
+    PageEditOrderComponent,
   ],
-  imports: [
-    CommonModule,
-    OrdersRoutingModule
-  ]
+  imports: [CommonModule, OrdersRoutingModule],
 })
-export class OrdersModule { }
+export class OrdersModule {}
