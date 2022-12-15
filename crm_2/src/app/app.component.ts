f5db674a955e0007d6a6e7613f52d58003068e32
PierreGau <p.gauthier09@gmail.com>
2022-12-15 13:57:13 +0100

    plein de trucs

--- crm_2/src/app/app.component.ts
+++ crm_2/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string;
+  //Observable (froid)
+  private obs: Observable<any>;
+
+  //observable (chaud)
+  private sbj: Subject<any>;
+
+  private behave: BehaviorSubject<any>;
+  private event: Event;
 
   constructor() {
     this.title = 'oui';
+    this.event = new Event('Suppression');
+
+    //--------- Observable froid ---------
+    this.obs = new Observable<any>((listXSubscribers) => {
+      listXSubscribers.next(Math.random());
+    });
+    this.obs.subscribe((data) => console.log(data));
+    this.obs.subscribe((data) => console.log(data));
+    //--------------------------------------
+
+    //--------- Observable chaud ---------
+    this.sbj = new Subject<any>();
+
+    this.sbj.subscribe((data) => console.log(data));
+    this.sbj.next('toto');
+    this.sbj.subscribe((data) => console.log(data));
+    this.sbj.next('toto2');
+    //-------------------------------------
+
+    this.behave = new BehaviorSubject<any>('oui');
+
+    this.behave.next('toto2');
+    this.behave.subscribe((data) => console.log(data));
+    this.behave.next(Math.random());
+    this.behave.subscribe((data) => console.log(data));
   }
 }
